72433947c878dfa164648e098ec30e55e312f4d0
kubilay1994 <kubilay.guran@gmail.com>
2020-09-24 17:32:40 +0300

    model güncellendi

--- model.py
+++ model.py
@@ -1,7 +1,10 @@
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D, BatchNormalization, Activation, Dense, Dropout
 from tensorflow.keras import Sequential
 
+from tensorflow.keras.applications import VGG16, InceptionV3
 
+
+InceptionV3()
 input_shape = 48, 48, 1
 
 
@@ -23,24 +26,24 @@ def facial_emotion_model_block(model, filters, kernel_size, input_shape=None):
     model.add(BatchNormalization())
     model.add(Activation("relu"))
 
-    model.add(MaxPooling2D())
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
 
 def facial_emotion_model():
     model = Sequential()
-    # block 1
     facial_emotion_model_block(model, 32, 3, input_shape=input_shape)
-    # block 2
-    facial_emotion_model_block(model, 64, 5)
-    # block 3
+    facial_emotion_model_block(model, 64, 3)
     facial_emotion_model_block(model, 128, 3)
-    # block 4
+
+    facial_emotion_model_block(model, 192, 3)
     facial_emotion_model_block(model, 256, 3)
 
     # final block
     model.add(GlobalAveragePooling2D())
     model.add(Dropout(0.3))
 
+    model.add(Dense(1024, activation="relu"))
+    model.add(Dropout(0.3))
     model.add(Dense(256, activation="relu"))
     model.add(Dropout(0.3))
     model.add(Dense(7, activation="softmax"))
@@ -48,6 +51,6 @@ def facial_emotion_model():
     return model
 
 
-
-
-    
+if __name__ == "__main__":
+    model = facial_emotion_model()
+    model.summary()
